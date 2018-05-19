e93cfe1a5423907091a3072eac8d2217fb1eff0a
Victor Avasiloaei <victor.avasiloaei@gmail.com>
2018-05-19 10:53:57 +0200

    Hardcode production urls

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  FRONTEND_URL: '"https://cinestat.avasiloaei.com/"',
-  BACKEND_URL: '"https://cinestat.avasiloaei.com/api/'
+  FRONTEND_URL: '"https://cinestat.victor.avasiloaei.com/"',
+  BACKEND_URL: '"https://cinestat.victor.avasiloaei.com/api/'
 }
